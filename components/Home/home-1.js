b154f553f0adf6ca2236f4785e70619f32c7a699
hellowebhelp <ashitmondal77@gmail.com>
2024-10-30 16:58:06 +0600

    First commitfgt

--- components/Home/home-1.js
+++ components/Home/home-1.js
@@ -3,17 +3,7 @@
  */
 
 import React from "react";
-import HomeStyle from '../Home/styles.module.css';
-import { Button } from "@nextui-org/react";
-import { Avatar, AvatarGroup } from "@nextui-org/react";
-import { CgArrowTopRight } from "react-icons/cg";
-import { BiCheckCircle } from "react-icons/bi";
-import { AiFillGoogleCircle } from "react-icons/ai";
-import { AiFillApple } from "react-icons/ai";
-import Image_1 from '../Home/image-1.webp';
-import { CircularProgress } from "@nextui-org/react";
-// import { Card, CardHeader, CardBody, CardFooter, Divider } from "@nextui-org/react";
-import { Progress } from "@nextui-org/react";
+
 
 
 
